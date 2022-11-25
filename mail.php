a7cd9c32767c69e56fb58ff4f7d2709255db6a2a
frobry <bryanfroehlich64@gmail.com>
2022-11-25 11:06:20 +0100

    Original Update
    First update 2022

---
+++ mail.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Envoi d'un message par formulaire</title>
+</head>
+
+<body>
+    <?php
+    $retour = mail('destinataire@free.fr', 'Envoi depuis la page Contact', $_POST['message'], 'From: webmaster@monsite.fr');
+    if ($retour)
+        echo '<p>Votre message a bien été envoyé.</p>';
+    ?>
+</body>
+</html>
