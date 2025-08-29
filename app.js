ac6d4f02f4ac77c81b5f91a931f4c4d53edba723
Bryan Froehlich <119042842+frobry@users.noreply.github.com>
2025-08-29 19:49:20 +0200

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-$(document).on("click", "a", function (event) {
-  event.preventDefault();
-  const href = $(this).attr("href");
-  $(".page-transition").addClass("fade-out");
-  setTimeout(function () {
-    window.location = href;
-  }, 500); // Temps de la transition en millisecondes
-});
